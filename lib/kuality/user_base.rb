e74f4e21794bf0c4ffbd833e57e3ab0cd3daada8
Abraham Heward <aheward@rsmart.com>
2012-12-21 06:35:18 -0700

    More tweaks to match UI changes.

--- lib/kuality/user_base.rb
+++ lib/kuality/user_base.rb
@@ -2,19 +2,23 @@ class UserBase < BasePage
 
   header_elements
 
-  def self.profile_elements
-    element(:name) { |b| b.text_field(id: "user_name") }
-    element(:username) { |b| b.text_field(id: "user_username") }
-    element(:email) { |b| b.text_field(id: "user_email") }
-    value(:name_error) { |b| b.div(class: /control-group string/).span(class: "help-inline").text }
-    value(:email_error) { |b| b.div(class: /control-group email/).span(class: "help-inline").text }
-  end
+  class << self
 
-  def password_elements
-    element(:current_password) { |b| b.text_field(id: "user_current_password") }
-    element(:password) { |b| b.text_field(id: "user_password") }
-    element(:password_confirmation) { |b| b.text_field(id: "user_password_confirmation") }
-    value(:password_error) { |b| b.div(class: /control-group password/).span(class: "help-inline").text }
+    def profile_elements
+      element(:name) { |b| b.text_field(id: "user_name") }
+      element(:username) { |b| b.text_field(id: "user_username") }
+      element(:email) { |b| b.text_field(id: "user_email") }
+      value(:name_error) { |b| b.div(class: /control-group string/).span(class: "help-inline").text }
+      value(:email_error) { |b| b.div(class: /control-group email/).span(class: "help-inline").text }
+    end
+
+    def password_elements
+      element(:current_password) { |b| b.text_field(id: "user_current_password") }
+      element(:password) { |b| b.text_field(id: "user_password") }
+      element(:password_confirmation) { |b| b.text_field(id: "user_password_confirmation") }
+      value(:password_error) { |b| b.div(class: /control-group password/).span(class: "help-inline").text }
+    end
+
   end
 
 end
