0dd100f8c5acb859c73295b10f02bd5c777f240d
Abraham Heward <aheward@rsmart.com>
2013-01-09 12:10:52 -0700

    Forgot the actions.

--- lib/kuality/base_page.rb
+++ lib/kuality/base_page.rb
@@ -3,10 +3,11 @@ class BasePage < PageFactory
   class << self
 
     def header_elements
-      action(:sign_out) { |b| b.link(text: %|" Sign out"|).click }
-      action(:reports) { |b| b.link(text: %|" Reports"|).click }
-      action(:register) { |b| b.link(text: %|" Register"|).click }
-      action(:home) { |b| b.link(text: %|" Home"|).click }
+      action(:sign_out) { |b| b.link(id: "navbar-menu-link-signout").click }
+      action(:sign_in) { |b| b.link(id: "navbar-link-signin").click }
+      action(:reports) { |b| b.link(id: "navbar-link-reports").click }
+      action(:register) { |b| b.link(id: "navbar-link-register").click }
+      action(:home) { |b| b.link(text: /Home/).click }
       element(:user_menu) { |b| b.link(class: "dropdown-toggle") }
       action(:loading) { |b| b.div(class: "loading hide").wait_while_present }
       value(:alert_box) { |b| b.div(class: /alert/).text }
